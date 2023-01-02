d90b8140a3bfadb507848640503a5e9418672c07
tenguri-jeon <110875363+tenguri-jeon@users.noreply.github.com>
2023-01-02 18:06:12 +0900

    fff

---
+++ js/event-hbaf.js
@@ -0,0 +1,50 @@
+
+    // const conTents = document.getElementsByClassName('contents');
+    // const contentsWrapper = document.getElementsByClassName('event-slidewrapper');
+    // const slidewrapper = document.getElementsByClassName('contents-wrapper');
+    // const leftButton = document.getElementsByClassName('before-contents-arrow')
+    // const rightButton = document.getElementsByClassName('next-contents-arrow')
+    
+    // const count = 4;
+    // let slideWith;
+    // let containerWidth;
+    // let index = 0;
+    
+
+    // leftButton[0].addEventListener('click', (e) => {
+    //     e.preventDefault();
+    //     leftslide();
+    // })
+
+    // rightButton[0].addEventListener('click', (e)=> {
+    //     e.preventDefault();
+    //     rifgtslide();
+    // })
+
+
+    // function checkWidth() {
+    //     containerWidth = contentsWrapper[0].clientWidth;
+    //     slideWith = (containerWidth - (20 * (count-1))) / count;    
+    //     for (let i = 0; i <conTents.length; i++) {
+    //         conTents[i].style.width = `${slideWith}px`;
+    //     }
+    // }
+
+
+    // function leftslide () {
+    //     if(index > 0) {
+    //         index--;
+    //     }
+    //     contentsSize();
+    // }
+
+    // function rifgtslide() {
+    //     if(index < slidewrapper.childElementCount - count){
+    //         index++
+    //     }
+    //     index++;
+    // }
+
+    // function contentsSize() {
+    //     slidewrapper[0].style.transform = `translateX(-${index * (slideWith + 20)}px)`;
+    // }
---
+++ js/header.js
@@ -0,0 +1,55 @@
+headerMenu();
+
+
+function headerMenu() {
+    const header = document.querySelector('.hbaf-header');
+    const headerNav = document.querySelector('.menu-tittle-wrapper');
+    const changeColor = document.getElementsByClassName('translate-color');
+    headerNav.addEventListener('mouseenter', ()=> {
+        header.classList.add('header-active');
+        for (let i = 0; i < changeColor.length; i++) {
+            changeColor[i].classList.add('color');
+        }
+    })
+    
+    headerNav.addEventListener('mouseleave', ()=>{
+        header.classList.remove('header-active')
+    })
+}
+
+// const header = document.querySelector('.hbaf-header');
+// let 이전스크롤좌표 = window.screenY;
+
+// window.addEventListener('scroll', (e)=>{
+//     console.log(window.scrollY);
+//     if(window.scrollY > 68){
+//         스크롤안된해더();
+//     }
+//     else{
+//         스크롤안된해더();
+//     }
+
+//     if(이전스크롤좌표 < window.scrollY){
+//         스크롤사라짐();
+//     }
+//     else{
+//         스크롤나타날();
+//     }
+// })
+
+// function 스크롤된해더(){
+//     header.classList.add('scrolled');
+// }
+
+// function 스크롤안된해더(){
+//     header.classList.remove('scrolled');
+// }
+
+// function 스크롤사라짐() {
+//     header.classList.add('hide-header');
+// }
+
+// function 스크롤나타날(){
+//     header.classList.remove('hide-header')
+// }
+
