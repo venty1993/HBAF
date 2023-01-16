379840152f7c485bda6ea7f3c8c2711a66652f7b
tenguri-jeon <110875363+tenguri-jeon@users.noreply.github.com>
2023-01-16 11:21:18 +0900

    f

--- js/animation.js
+++ js/animation.js
@@ -28,7 +28,21 @@ function orginalLinetext() {
     
 }
 
+let observer = new IntersectionObserver ((e)=>{
+    e.forEach((박스)=>{
+        if(박스.isIntersecting){
+        박스.target.classList.add();
+        } else{
+        박스.target.classList.add();
+    }
+})
+})
+
 let choiceR = document.querySelectorAll('div');
+observer.observe(choiceR[70]);
+observer.observe(choiceR[73]);
+observer.observe(choiceR[78]);
+observer.observe(choiceR[81]);
 
 function backgroundcontrol() {
     
